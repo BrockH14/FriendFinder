b2216341e46326a9bf797426ffd8311642baf9e6
BrockH <brock.horton@gmail.com>
2020-05-03 12:59:04 -0400

    update server

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ var PORT = process.env.port || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./routing/apiRoutes")(app);
-require("./routing/htmlRoutes")(app);
+require("./routing/apiRoutes.js")(app);
+require("./routing/htmlRoutes.js")(app);
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
