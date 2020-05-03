16c4cd8b7481c8eab1f174f76c02589c072f0e66
BrockH <brock.horton@gmail.com>
2020-05-02 20:31:53 -0400

    functionality update, final tests and polish nedded

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,27 @@
-var userData = {
-    name: "John Smith",
-    photo: "https://media.istockphoto.com/photos/happy-laughing-man-picture-id544358212?k=6&m=544358212&s=612x612&w=0&h=odURMNz2hty8LIfpVahaaUKpGU4vd-UlZx4jy-OAnJA=",
+var userData = [{
+    name: "Moana",
+    photo: "https://cdn.shortpixel.ai/client/to_webp,q_glossy,ret_img,w_924,h_924/https://kidonthetown.com/wp-content/uploads/bb-plugin/cache/lenfest-kids-moana-e1579839002860-square.jpg",
     scores: [1,1, 1, 1, 1, 1, 1, 1, 1, 1]
-    }
+    },
+    {
+    name: "Rafiki",
+    photo: "https://miro.medium.com/max/850/1*TrcdaUIQKlaTQrf0YponxQ.jpeg",
+    scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+    },
+    {
+    name: "Robin Hood",
+    photo: "https://66.media.tumblr.com/e8a227bd143cee3baeff869f92d05fe7/tumblr_payjtvGNrf1x24vklo2_1280.jpg",
+    scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    },
+    {
+    name: "Thomas O'malley",
+    photo: "https://vignette.wikia.nocookie.net/disney/images/7/70/Profile_-_Thomas_O%27_Malley.jpeg/revision/latest/scale-to-width-down/516?cb=20190426050232",
+    scores: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+    },
+    {
+    name: "Nani",
+    photo: "https://thedisneywoman.files.wordpress.com/2015/10/nani.jpg",
+    scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+    },
+]
 module.exports = userData;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,16 +1,35 @@
-var tableData = require("../data/answerData");
+var tableData = require("../data/friends.js");
 
 module.exports = function(app) {
 
-app.get("/api/answers", function(req, res) {
-    res.json(tableData);
+app.get('/api/friends', function(req, res) {
+    res.json(userData);
 });
-app.post("/api/answers", function(req, res) {
-      tableData.push(req.body);
-      res.json(true);
+app.post('/api/friends', function (req, res) {
+    var diff = 40;
+    var matchName = '';
+    var matchPhoto = '';
+    tableData.forEach(function (userData) {
+        var matchScoreArr = [];
+        var totalDiff = 40;
+        function add(total, num) {
+            return total + num;
+        }
+        for (var i = 0; i < userData.scores.length; i++) {
+            matchScoreArr.push(Math.abs(parseInt(req.body.scores[i]) - parseInt(userData.scores[i])));
+        }
+        totalDiff = matchScoreArr.reduce(add, 0);
+        if (totalDiff < diff) {
+            diff = totalDiff;
+            matchName = userData.name;
+            matchPhoto = userData.photo;
+        }
+    });
+    res.json({
+        name: matchName,
+        photo: matchPhoto
+    });
+    tableData.push(req.body);
 });
-app.post("/api/clear", function(req, res) {
-    tableData.length = 0;
-    res.json({ ok: true });
-  });
-};
+}
+
